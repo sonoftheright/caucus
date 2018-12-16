27cba3393f25648cf5c1e8ee80085c9fe0f57227
Benjamin Marquardt <son.of.the.right@gmail.com>
2018-12-16 15:24:09 -0500

    Added guest recognition, guest data encapsulation, and local chat log storage and recall.

--- public/js/client.js
+++ public/js/client.js
@@ -2,10 +2,31 @@
 
 var socket = io();
 
-var self = { "name": "", "id": ""};
+var self = {"name": "", "id": ""};
 
 var form = document.getElementById("form");
 var chatBox = document.getElementById("m");
+var msgHistoryLimit = 1000000; //1,000,000
+
+function addMessageToDisplay(msg)
+{
+	let newMessage = document.createElement('li');
+	newMessage.innerHTML = msg; 
+	document.getElementById("messages").append(newMessage);	
+}
+
+msgLog = localforage.getItem("msgLog", (err, value) => {
+	if(value)
+	{
+		var trimmedMsgLog = [];
+		for(var x = 0; x < value.length && x < msgHistoryLimit; x++)
+		{
+			addMessageToDisplay(value[x]);
+			trimmedMsgLog.push(value[x]);
+		}
+		msgLog = trimmedMsgLog;
+	}
+});
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -22,9 +43,18 @@ socket.on("login", (data) => {
 });
 
 socket.on('chat message', (msg) => {
-	let newMessage = document.createElement('li');
+	addMessageToDisplay(msg);
+	msgLog.push(msg);
+	localforage.setItem("msgLog", msgLog, (err, value) => {
+		if(err)
+		{
+			console.log("ERROR: " + err);
+		}
+	});
+});
 
-	newMessage.innerHTML = msg; 
-
-	document.getElementById("messages").append(newMessage);
+socket.on('force refresh', () =>
+{
+	console.log("Refreshing. . .");
+	location.reload(true);
 });
--- server.js
+++ server.js
@@ -2,33 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let users = [];
+let users = {};
 var request = {};
 
 var guestcount = 100000; 			//100,000
 
-function newUser(name, ip, userID)
+function newUser(ip, userID)
 {
-	for(var x = 0; x < users.length; x++)
-	{
-		if(users[x].ip == ip)
-		{
-			return users[x];
-		}
-	}
+	if(users[ip]) { return users[ip]; console.log("User with same IP found: " + ip); }
 
-	var user = { "name": name, "ip": ip, "userID": userID };
-	users.push(user);
+	var user = { "name": "guest" + guestcount++, "ip": ip, "userID": userID };
+	users[ip] = user;
 	return user;
 }
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-  request = req;
+  	res.sendFile(__dirname + '/public/index.html');
+  	request = req;
 });
 
 app.get('/client.js', function(req, res) {
 	res.sendFile(__dirname + "/public/js/client.js");
+	request = req;
+});
+
+app.get('/localforage.js', function(req, res) {
+	res.sendFile(__dirname + "/public/js/localforage.js");
 	request = req;
 });
 
@@ -38,17 +37,28 @@ app.get('/styles.css', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-	var user = newUser("guest" + guestcount++, request.ip, socket.id);
+	if(!request.ip)
+	{
+		console.log("Forcing refresh for user on socket " + socket.id);
+		socket.emit('force refresh');
+	}
+
+	if(!users[request.ip])
+	{
+		io.emit('chat message', "SYSTEM: User has connected on " + request.ip);
+	}
+
+	var user = newUser(request.ip, socket.id);
 
 	console.log("user " + user.name + " at " + user.ip + " using socket " + user.userID + " has connected");
 
-	socket.broadcast.emit("login", user);
+	socket.emit("guestlogin", user);
 
 	socket.on('chat message', (msg) => {
 		io.emit('chat message', user.name + ": " + msg);
 	});
 
-	socket.on('disconnect', () => { 
+	socket.on('disconnect', () => {
 		console.log("user " + user.name + " at " + user.ip + " using socket " + user.userID + " has disconnected"); 
 	}); 
 });
