57d40c023e145e2b42c4d1b2a358705aebfaa7a5
Benjamin Marquardt <son.of.the.right@gmail.com>
2018-12-19 13:00:34 -0500

    Aesthetic edits - guest list, typing - and functionality improvements

--- public/js/client.js
+++ public/js/client.js
@@ -7,25 +7,77 @@ var self = {"name": "", "id": ""};
 var form = document.getElementById("form");
 var chatBox = document.getElementById("m");
 var msgHistoryLimit = 1000000; //1,000,000
+var idleTime = 10000;
+
+var guestList = {};
+
+function getTimestamp() 
+{
+	var d, day, month, year, hours, minutes, seconds;
+	d = new Date(); 
+	day = d.getDate();
+	month = d.getMonth() + 1;
+	year = d.getFullYear();
+	hours = d.getUTCHours() - (d.getTimezoneOffset() / 60);
+	minutes = d.getMinutes();
+	seconds = d.getSeconds();
+	
+	return month + "/" 
+			+ day + "/" 
+			+ year + ":("
+			+ hours + ":"
+			+ minutes + ":"
+			+ seconds + "):";
+}
 
 function addMessageToDisplay(msg)
 {
+	if(msg.length < 1) { return; }
+	
 	let newMessage = document.createElement('li');
 	newMessage.innerHTML = msg; 
 	document.getElementById("messages").append(newMessage);	
 }
 
-msgLog = localforage.getItem("msgLog", (err, value) => {
+function updateGuestList(users)
+{
+//	console.log("Updating guest list: %s", JSON.stringify(users));
+	document.getElementById('guestlist').innerHTML = "";
+	for (var user in users)
+	{
+		if(document.getElementById(users[user]["name"])) { continue; }
+		var userNameDiv = document.createElement('div');
+		userNameDiv.className = "guest";
+		var userListItem = document.createElement('span');
+		userListItem.id = users[user]["userID"];
+		userListItem.innerHTML = users[user]["name"];
+		userListItem.typing = false;
+
+		userNameDiv.append(userListItem);
+
+		if(users[user].typing) { 
+			var typingEllipsis = document.createElement('span');
+			typingEllipsis.className = "typing";
+			typingEllipsis.innerHTML = " &#8230; ";
+			userNameDiv.append(typingEllipsis);
+		}
+
+		document.getElementById('guestlist').append(userNameDiv);
+	}
+}
+
+var msgLog = [];
+localforage.getItem("msgLog", (err, value) => {
+	var trimmedMsgLog = [];
 	if(value)
 	{
-		var trimmedMsgLog = [];
 		for(var x = 0; x < value.length && x < msgHistoryLimit; x++)
 		{
 			addMessageToDisplay(value[x]);
 			trimmedMsgLog.push(value[x]);
 		}
-		msgLog = trimmedMsgLog;
 	}
+	msgLog = trimmedMsgLog;
 });
 
 form.addEventListener('submit', (e) => {
@@ -36,19 +88,29 @@ form.addEventListener('submit', (e) => {
 	console.log('message sent.');
 });
 
+chatBox.addEventListener('input', (e) => {
+	idleTime = 0;
+	socket.emit('typing');
+});
+
 socket.on("guestlogin", (data) => {
 	console.log("New user data: " + JSON.stringify(data));
-	self.name = data.name;
-	self.id = data.id;
+	guestList = data;
+	updateGuestList(data);
+});
+
+socket.on("guestlistupdate", (users) => {
+	updateGuestList(users);
 });
 
 socket.on('chat message', (msg) => {
+	msg = getTimestamp() + msg;
 	addMessageToDisplay(msg);
 	msgLog.push(msg);
 	localforage.setItem("msgLog", msgLog, (err, value) => {
 		if(err)
 		{
-			console.log("ERROR: " + err);
+			console.log("ERROR SETTING ITEM: " + err);
 		}
 	});
 });
@@ -58,3 +120,20 @@ socket.on('force refresh', () =>
 	console.log("Refreshing. . .");
 	location.reload(true);
 });
+
+function clearCache()
+{
+	console.log("Clearing entire cache at request of server.");
+	localforage.clear((err) => {
+		console.log("ERROR CLEARING CACHE: " + err);
+	});
+}
+
+socket.on('force clear cache', () => {
+	clearCache();
+});
+
+function forceClearAll()
+{
+	socket.emit('force clear all');
+}
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 let users = {};
+let guestlist = {};
 var request = {};
+
+var system = "<strong>SYSTEM</strong>";
 
 var guestcount = 100000; 			//Max guest count: 100,000
 
@@ -35,12 +38,13 @@ app.get('/localforage.js', (req, res) => {
 
 app.get('/styles.css', (req, res) => {
 	request = req;
-	res.sendFile(__dirname + "/public/css/styles.css")
+	res.sendFile(__dirname + "/public/css/styles.css");
 });
 
-/* SOCKET CONNECTION PROTOCOL */
+/* SOCKET CONNECTION PROTOCOL - USER HANDLING*/
 
 io.on('connection', (socket) => {
+
 	/* 
 		This will happen if the server resets but the client is still on the page. The 
 		client will establish a reconnect without sending a GET request first, so 
@@ -53,32 +57,88 @@ io.on('connection', (socket) => {
 	}
 
 	/*
-		Guests are inserted into the 
+		TODO:
+		Guests are inserted into the local storage object according to their ip address.
+		Users coming from the same source - such as those behind the same router - might 
+		make things confusing later, so this should probably be made more robust.
 	*/
 
-	if(!users[request.ip])
+	// Check to make sure there's an ip address and it's not undefined
+	if(!users[request.ip] && request.ip !== undefined)
 	{
-		io.emit('chat message', "SYSTEM: User has connected on " + request.ip);
+		io.emit('chat message', system + ": User has connected on " + request.ip);
+	}
+	// if the ip is undefined, refresh and ditch - which should bring them back here
+	// with corrected data
+
+	else if( request.ip == undefined )
+	{
+		console.log('User on socket ' + socket.id + ' has invalid ip; force refreshing.');
+		socket.emit('force refresh');
+		return;
 	}
 
+	// establish server identity for use
 	var user = newUser(request.ip, socket.id);
 
-	console.log("user " + user.name + " at " + user.ip + " using socket " + user.userID + " has connected");
+	// add user to current guest list and log user details
+	guestlist[user.ip] = { "name" : user.name, "typing" : false };
 
-	socket.emit("guestlogin", user);
+	console.log("user " + user.name 
+				+ " at " + user.ip 
+				+ " using socket " + user.userID 
+				+ " has connected");
 
+	// update everyone's guest lists
+	io.emit("guestlistupdate", guestlist);
+
+	// when a chat message happens client-side, update all the things
 	socket.on('chat message', (msg) => {
 		io.emit('chat message', user.name + ": " + msg);
 	});
 
+	// when a client disconnects, update the clients' guest list but RETAIN
+	// USER IDENTITY
 	socket.on('disconnect', () => {
-		console.log("user " + user.name + " at " + user.ip + " using socket " + user.userID + " has disconnected"); 
-	}); 
+		delete guestlist[user.ip];
+		io.emit("guestlistupdate", guestlist);
+		io.emit("chat message", system +": User " + user.name + " has disconnected.");
+		console.log("user " + user.name 
+					+ " at " + user.ip 
+					+ " using socket " + user.userID 
+					+ " has disconnected"); 
+	});
+
+	socket.on('typing', () =>
+	{
+		guestlist[user.ip]["typing"] = true;
+		io.emit("guestlistupdate", guestlist);
+		setTimeout(() => {
+			guestlist[user.ip]["typing"] = false;
+			io.emit("guestlistupdate", guestlist);
+		}, 1000);
+	});
 });
 
 /* RUN SERVER */
 
-
 http.listen(80, function(){
   console.log('server is up.');
 });
+
+
+/*
+TODO (from notes):
+
+- Store list of connected clients in database
+	- Serve list on request or on change
+- Establish 'rooms', each with its own database storing the above
+
+
+Room data:
+	- clients
+		- Name
+		- Timestamp joined
+		- Idle?
+		- Typing? 
+*/
